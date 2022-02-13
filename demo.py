697fbca45523afc4ccefa6ad064e030383103f40
adichiara <adamdichiara@gmail.com>
2022-02-13 17:15:15 -0500

    added goodbye

--- demo.py
+++ demo.py
@@ -3,7 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+@app.route("/hello")
+@app.route("/hello/<name>")
+def hello(name=''):
 
-    return "Hello there:"
+    return "Hello there:" + name
 
+@app.route("/goodbye/<name>")
+def goodbye(name=''):
+
+    return "see ya:" + name
+
+
+if __name__ == '__main__':
+  
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
