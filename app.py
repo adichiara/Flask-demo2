0faf9684003ce343907253fae74634a7b8c48643
adichiara <adamdichiara@gmail.com>
2022-02-13 17:27:57 -0500

    renamed app.py

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/hello")
+@app.route("/hello/")
 @app.route("/hello/<name>")
 def hello(name=''):
 
-    return "Hello there:" + name
+    return "Hello there: " + name
 
-@app.route("/goodbye/<name>")
-def goodbye(name=''):
+# @app.route("/goodbye/<name>")
+# def goodbye(name=''):
 
-    return "see ya:" + name
+#     return "see ya: " + name
 
 
 if __name__ == '__main__':
