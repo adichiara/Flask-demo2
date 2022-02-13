bc31315a2479b16baf48df6ac71906c49dabe043
adichiara <adamdichiara@gmail.com>
2022-02-13 17:39:18 -0500

    whatever

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/hello/")
-@app.route("/hello/<name>")
+# @app.route("/hello/")
+# @app.route("/hello/<name>")
 def hello(name=''):
 
     return "Hello there: " + name
